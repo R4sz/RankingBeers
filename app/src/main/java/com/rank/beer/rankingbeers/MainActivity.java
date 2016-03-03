29bffeaea184999bb95f515790b7fc0dd2333c41
Marcin Skrzypiec <mm.skrzypiec@gmail.com>
2016-03-03 22:50:17 +0100

    added view beer (partly)

--- app/src/main/java/com/rank/beer/rankingbeers/MainActivity.java
+++ app/src/main/java/com/rank/beer/rankingbeers/MainActivity.java
@@ -1,14 +1,48 @@
 package com.rank.beer.rankingbeers;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    protected static SparseArray<Class<? extends Activity>> buttonConfig = new SparseArray<Class<? extends Activity>>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonConfig.append(R.id.addBeer, AddBeerActivity.class);
+        initButtons();
 
     }
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.exit) {
+            moveTaskToBack(true);
+            return;
+        }
+
+        Intent i = new Intent(this, buttonConfig.get(v.getId()));
+        startActivity(i);
+    }
+
+    void initButtons() {
+        int key = 0;
+        View view = null;
+        for (int i = 0; i < buttonConfig.size(); i++) {
+            key = buttonConfig.keyAt(i);
+            view = findViewById(key);
+            view.setOnClickListener(this);
+        }
+
+        view = findViewById(R.id.exit);
+        view.setOnClickListener((View.OnClickListener) this);
+    }
+
 }
+
